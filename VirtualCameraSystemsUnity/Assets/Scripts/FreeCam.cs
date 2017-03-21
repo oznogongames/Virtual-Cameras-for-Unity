a4239a545ab51a54f7ec40330a893c8fc83c0355
JasperDre <jasperdre@gmail.com>
2017-03-21 15:54:26 +0100

    Added basic camera and environment

---
+++ VirtualCameraSystemsUnity/Assets/Scripts/FreeCam.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+public class FreeCam : MonoBehaviour
+{
+    float mainSpeed = 100.0f;   //Regular speed variable
+    float shiftAdd = 250.0f;    //Additional force variable
+    float maxShift = 1000.0f;   //Maximum additional force variable
+    float camSens = 0.25f;      //Mouse sensitivity variable
+    private Vector3 previousMousePosition = new Vector3(255, 255, 255); //3D vector for previous mouse position
+    private float totalRun = 1.0f; //Total runtime variable
+
+    void Update()
+    {
+        //Mouse input
+        previousMousePosition = Input.mousePosition - previousMousePosition;
+        previousMousePosition = new Vector3(-previousMousePosition.y * camSens, previousMousePosition.x * camSens, 0);
+        previousMousePosition = new Vector3(transform.eulerAngles.x + previousMousePosition.x, transform.eulerAngles.y + previousMousePosition.y, 0);
+        transform.eulerAngles = previousMousePosition;
+        previousMousePosition = Input.mousePosition;
+
+        //Keyboard input
+        Vector3 p = GetBaseInput();
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            totalRun += Time.deltaTime;
+            p = p * totalRun * shiftAdd;
+            p.x = Mathf.Clamp(p.x, -maxShift, maxShift);
+            p.y = Mathf.Clamp(p.y, -maxShift, maxShift);
+            p.z = Mathf.Clamp(p.z, -maxShift, maxShift);
+        }
+        else
+        {
+            totalRun = Mathf.Clamp(totalRun * 0.5f, 1f, 1000f);
+            p = p * mainSpeed;
+        }
+
+        p = p * Time.deltaTime;
+        Vector3 newPosition = transform.position;
+
+        if (Input.GetKey(KeyCode.Space))
+        { //Move on X and Z axis only
+            transform.Translate(p);
+            newPosition.x = transform.position.x;
+            newPosition.z = transform.position.z;
+            transform.position = newPosition;
+        }
+        else
+        { //Move all axes
+            transform.Translate(p);
+        }
+    }
+
+    private Vector3 GetBaseInput()
+    {
+        Vector3 p_Velocity = new Vector3();
+        if (Input.GetKey(KeyCode.W))
+        {
+            p_Velocity += new Vector3(0, 0, 1);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            p_Velocity += new Vector3(0, 0, -1);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            p_Velocity += new Vector3(-1, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            p_Velocity += new Vector3(1, 0, 0);
+        }
+        return p_Velocity; //Returns the basic values, if it's 0 than it's not active.
+    }
+}
---
+++ VirtualCameraSystemsUnity/Assets/Scripts/RTSCamera.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class RTSCamera : MonoBehaviour
+{
+    public float scrollZone = 30.0f;    //Detection zone variable
+    public float scrollSpeed = 5.0f;    //Scroll speed variable
+
+    public float xMax = 8.0f;           //Maximum x value of the camera
+    public float xMin = 0.0f;           //Minimum x value of the camera
+    public float yMax = 10.0f;          //Maximum y value of the camera
+    public float yMin = 3.0f;           //Minimum y value of the camera
+    public float zMax = 8.0f;           //Maximum z value of the camera
+    public float zMin = 0.0f;           //Minimum z value of the camera
+
+    private Vector3 desiredPosition;    //3D vector for the desired position of the camera
+
+    //Start method is called at initiation
+    private void Start()
+    {
+        desiredPosition = transform.position; //Store initial position into desired position for reference
+    }
+
+    //Update is called once per frame
+    private void Update()
+    {
+        float x = 0, y = 0, z = 0;  //3D vector variables
+        float speed = scrollSpeed;  //Speed variable
+
+        if (Input.mousePosition.x < scrollZone)
+        {
+            x -= speed;
+        }
+        else if (Input.mousePosition.x > Screen.width - scrollZone)
+        {
+            x += speed;
+        }
+
+        if (Input.mousePosition.y < scrollZone)
+        {
+            z -= speed;
+        }
+        else if (Input.mousePosition.y > Screen.height - scrollZone)
+        {
+            z += speed;
+        }
+
+        y += Input.GetAxis("Mouse ScrollWheel") * speed * 5.0f; //Scrollwheel input of the mouse for Y movement
+
+        Vector3 move = new Vector3(x, -y, z) + desiredPosition;
+        move.x = Mathf.Clamp(move.x, xMin, xMax);
+        move.y = Mathf.Clamp(move.y, yMin, yMax);
+        move.z = Mathf.Clamp(move.z, zMin, zMax);
+        desiredPosition = move;
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, 0.2f);
+    }
+}
