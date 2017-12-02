5704ead280cd44ac551f111b5f924a8d8d99bb34
JasperDre <jasperdre@gmail.com>
2017-12-02 22:51:59 +0100

    Cleaned up RPGCamera (WIP)
    Possible rotation issues and offset should be smoother.

--- Virtual Cameras/Assets/Scripts/RPGCamera.cs
+++ Virtual Cameras/Assets/Scripts/RPGCamera.cs
@@ -1,24 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class RPGCamera : MonoBehaviour
 {
-    public Transform target; 		    //Target position and rotation
-    public float offsetX = 0.0f;        //Offset on x-axis
-    public float offsetY = 1.0f;        //Offset on y-axis
-    public float offsetZ = 1.0f;
-    public float maxOffsetZ = 5.0f;
-    public float xSpeed = 250.0f;	    //Rotation speed on the x-axis
-    public float ySpeed = 120.0f;       //Rotation speed on the y-axis
-    public float zSpeed = 120.0f;       //Rotation speed on the z-axis
-    public float yMinRotation = -90.0f;    //Least possible value of the y rotation
-    public float yMaxRotation = 90.0f;		//Maximum possible value of the y rotation
+    [Header("Target")]
+    [Tooltip("Reference to the target GameObject")]
+    public Transform target;
+    [Tooltip("Relative offset from the target position")]
+    public Vector3 offset;
+    [Tooltip("Maximum relative offset from the target position")]
+    public Vector3 maxOffset;
+    [Header("Camera")]
+    [Tooltip("Rotation speed")]
+    public Vector2 rotationSpeed;
+    [Tooltip("Rotation limits for the Y-axis")]
+    public Vector2 rotationLimits;
+    [Tooltip("Speed scalar for the mouse wheel")]
     public float scrollSpeed = 25.0f;
-    public bool canChangeY = true;
-    private float x;				    //X position value
-    private float y;				    //Y position value
-    private float z = 3.0f;
+    [Tooltip("Whether the player can change the rotation on the Y-axis of the camera")]
+    public bool canChangeYAxis = true;
+
+    private Vector3 position;
 
     //Use this for initialization
     void Start()
@@ -34,31 +35,28 @@ public class RPGCamera : MonoBehaviour
     {
         if (target && Input.GetMouseButton(1))
         {
-            x += Input.GetAxis("Mouse X") * xSpeed * 0.02f; //Update X-axis based on mouse input
-            if (canChangeY)
+            position.x += Input.GetAxis("Mouse X") * rotationSpeed.x * Time.deltaTime;
+            if (canChangeYAxis)
             {
-                y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f; //Update Y-axis based on mouse input
+                position.y -= Input.GetAxis("Mouse Y") * rotationSpeed.y * Time.deltaTime;
             }
-            y = Mathf.Clamp(y, yMinRotation, yMaxRotation);       //Update the current Y-axis value by interpolating it between the limit
+            position.y = Mathf.Clamp(position.y, rotationLimits.x, rotationLimits.y);
         }
 
-        if (transform.position.z > target.transform.position.z - offsetZ)
+        if (transform.position.z > target.transform.position.z - offset.z)
         {
-            z = target.transform.position.z + offsetZ;
+            position.z = target.transform.position.z + offset.z;
         }
-        else if (transform.position.z < target.transform.position.z - maxOffsetZ)
+        else if (transform.position.z < target.transform.position.z - maxOffset.z)
         {
-            z = target.transform.position.z + maxOffsetZ;
+            position.z = target.transform.position.z + maxOffset.z;
         }
         else
         {
-            z -= Input.GetAxis("Mouse ScrollWheel") * Time.fixedDeltaTime * scrollSpeed;
+            position.z -= Input.GetAxis("Mouse ScrollWheel") * Time.fixedDeltaTime * scrollSpeed;
         }
 
-        Quaternion rotation = Quaternion.Euler(y, x, 0.0f);  //Rotation euler angles
-        Vector3 position = rotation * new Vector3(offsetX, offsetY, -z) + target.position; //Position is current rotation/orientation multiplied by the target input
-
-        transform.rotation = rotation; //Assign new rotation
-        transform.position = position; //Assign new position
+        transform.rotation = Quaternion.Euler(position.y, position.x, 0.0f);
+        transform.position = transform.rotation * new Vector3(offset.x, offset.y, -position.z) + target.position;
     }
 }
