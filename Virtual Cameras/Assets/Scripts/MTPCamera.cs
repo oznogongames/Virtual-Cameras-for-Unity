5e4e33f41a744b11484bb40a0333c53ed74a5146
JasperDre <jasperdre@gmail.com>
2017-12-02 22:20:55 +0100

    Added basic cameras

---
+++ Virtual Cameras/Assets/Scripts/MTPCamera.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class MTPCamera : MonoBehaviour
+{
+    public float yOffset = 1.0f;
+    public float xSpeed = 250.0f;		//Rotation speed on the x-axis
+    public float ySpeed = 120.0f;       //Rotation speed on the y-axis
+    public float zSpeed = 120.0f;       //Rotation speed on the z-axis
+    public float yMinLimit = -90.0f;    //Least possible value of the y rotation
+    public float yMaxLimit = 90.0f;		//Maximum possible value of the y rotation
+    private float x;					//X position value
+    private float y;                    //Y position value
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) //Check for left mouse button press
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                gameObject.transform.position = hit.transform.position + new Vector3(0.0f, yOffset, 0.0f);
+            }
+        }
+
+        if (Input.GetMouseButton(1)) //Check for right mouse button press 
+        {
+            x += Input.GetAxis("Mouse X") * xSpeed * 0.02f; //Update X-axis based on mouse input
+            y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f; //Update Y-axis based on mouse input
+            y = Mathf.Clamp(y, yMinLimit, yMaxLimit);       //Update the current Y-axis value by interpolating it between the limit
+        }
+
+        Quaternion rotation = Quaternion.Euler(y, x, 0);   //Rotation euler angles
+
+        transform.rotation = rotation;  //Assign the new rotation/orientation
+    }
+}
---
+++ Virtual Cameras/Assets/Scripts/RPGCamera.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RPGCamera : MonoBehaviour
+{
+    public Transform target; 		    //Target position and rotation
+    public float offsetX = 0.0f;        //Offset on x-axis
+    public float offsetY = 1.0f;        //Offset on y-axis
+    public float offsetZ = 1.0f;
+    public float maxOffsetZ = 5.0f;
+    public float xSpeed = 250.0f;	    //Rotation speed on the x-axis
+    public float ySpeed = 120.0f;       //Rotation speed on the y-axis
+    public float zSpeed = 120.0f;       //Rotation speed on the z-axis
+    public float yMinRotation = -90.0f;    //Least possible value of the y rotation
+    public float yMaxRotation = 90.0f;		//Maximum possible value of the y rotation
+    public float scrollSpeed = 25.0f;
+    public bool canChangeY = true;
+    private float x;				    //X position value
+    private float y;				    //Y position value
+    private float z = 3.0f;
+
+    //Use this for initialization
+    void Start()
+    {
+        if (target == null)
+        {
+            Debug.LogWarning("No target found!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target && Input.GetMouseButton(1))
+        {
+            x += Input.GetAxis("Mouse X") * xSpeed * 0.02f; //Update X-axis based on mouse input
+            if (canChangeY)
+            {
+                y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f; //Update Y-axis based on mouse input
+            }
+            y = Mathf.Clamp(y, yMinRotation, yMaxRotation);       //Update the current Y-axis value by interpolating it between the limit
+        }
+
+        if (transform.position.z > target.transform.position.z - offsetZ)
+        {
+            z = target.transform.position.z + offsetZ;
+        }
+        else if (transform.position.z < target.transform.position.z - maxOffsetZ)
+        {
+            z = target.transform.position.z + maxOffsetZ;
+        }
+        else
+        {
+            z -= Input.GetAxis("Mouse ScrollWheel") * Time.fixedDeltaTime * scrollSpeed;
+        }
+
+        Quaternion rotation = Quaternion.Euler(y, x, 0.0f);  //Rotation euler angles
+        Vector3 position = rotation * new Vector3(offsetX, offsetY, -z) + target.position; //Position is current rotation/orientation multiplied by the target input
+
+        transform.rotation = rotation; //Assign new rotation
+        transform.position = position; //Assign new position
+    }
+}
